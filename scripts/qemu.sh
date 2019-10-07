00256c107c97608469035faf25482d82df594571
Jeremy Soller <jeremy@system76.com>
2019-10-07 12:15:03 -0600

    Increase qemu memory

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -17,7 +17,7 @@ then
 fi
 MODEL_DIR="$(realpath "models/${MODEL}")"
 
-kvm -M q35 -m 1024 -vga std \
+kvm -M q35 -m 4096 -vga std \
     -bios "build/${MODEL}/coreboot.rom" \
     -chardev stdio,mux=on,id=debug \
     -device isa-serial,index=2,chardev=debug \
