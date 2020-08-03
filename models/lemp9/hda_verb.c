5f9ae97b93df2175eb5abedb91ee56930a79040b
Tim Crawford <tcrawford@system76.com>
2020-08-03 09:22:32 -0600

    models: Update coreboot hda_verb.c files
    - Rename from hda.h
    - Update license block
    - Remove header guards

--- models/lemp9/hda_verb.c
+++ models/lemp9/hda_verb.c
@@ -1,20 +1,4 @@
-/*
- * This file is part of the coreboot project.
- *
- * Copyright (C) 2019 System76
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- * GNU General Public License for more details.
- */
-
-#ifndef HDA_VERB_H
-#define HDA_VERB_H
+/* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/azalia_device.h>
 
@@ -48,5 +32,3 @@ const u32 cim_verb_data[] = {
 const u32 pc_beep_verbs[] = {};
 
 AZALIA_ARRAY_SIZES;
-
-#endif
