efb612975b049b43f82a08449a5b41814e60ddeb
Jeremy Soller <jeremy@system76.com>
2020-12-01 14:36:25 -0700

    Fix error handling

--- you-ass-bee-see/src/main.rs
+++ you-ass-bee-see/src/main.rs
@@ -1,6 +1,7 @@
+use i2cdev::core::I2CDevice;
 use i2cdev::linux::LinuxI2CDevice;
 
 fn main() {
-    let mut dev = LinuxI2CDevice::new("/dev/i2c-11", 0x40);
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-11", 0x40).unwrap();
     println!("{:X?}", dev.smbus_read_block_data(0));
 }
