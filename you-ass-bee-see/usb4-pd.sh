06fb69ac8bd5d676bc0bca9e6b08d68061b8a542
Jeremy Soller <jeremy@system76.com>
2020-12-01 10:29:36 -0700

    Use coreboot-collector to set ROM_I2C_EN

--- you-ass-bee-see/usb4-pd.sh
+++ you-ass-bee-see/usb4-pd.sh
@@ -7,5 +7,17 @@ I2C_PCI_SYSFS="/sys/bus/pci/devices/0000:00:15.1"
 I2C_SYSFS="$(echo "${I2C_PCI_SYSFS}/i2c_designware."*"/i2c-"*)"
 I2C_DEV="/dev/$(basename "${I2C_SYSFS}")"
 
+cargo build \
+    --release \
+    --manifest-path ../tools/coreboot-collector/Cargo.toml \
+    --example usb4-pd
+sudo ../tools/coreboot-collector/target/release/examples/usb4-pd
+
 make -C eeprog-0.7.6-tear12
-sudo ./eeprog-0.7.6-tear12/eeprog "${I2C_DEV}" 0x50 -r 0 -o eeprom.rom
+sudo ./eeprog-0.7.6-tear12/eeprog \
+    "${I2C_DEV}" \
+    0x50 \
+    -r 0x0:0x40000 \
+    -o eeprom.rom
+
+xxd eeprom.rom | less
