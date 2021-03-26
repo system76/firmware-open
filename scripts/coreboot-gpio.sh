85d041db67deb2350b52690de1e46b6b7af4ca08
Tim Crawford <crawfxrd@gmail.com>
2021-03-26 08:36:04 -0600

    scripts: Remove extra indent when generating coreboot GPIO data

--- scripts/coreboot-gpio.sh
+++ scripts/coreboot-gpio.sh
@@ -71,28 +71,28 @@ do
 
     case "${parts[1]}" in
         0x??000000 | 0x??000200)
-            echo -e "\t\tPAD_CFG_TERM_GPO(${parts[0]}, 0, ${term}, ${reset}),"
+            echo -e "\tPAD_CFG_TERM_GPO(${parts[0]}, 0, ${term}, ${reset}),"
             ;;
         0x??000001 | 0x??000201)
-            echo -e "\t\tPAD_CFG_TERM_GPO(${parts[0]}, 1, ${term}, ${reset}),"
+            echo -e "\tPAD_CFG_TERM_GPO(${parts[0]}, 1, ${term}, ${reset}),"
             ;;
         0x??000100)
-            echo -e "\t\tPAD_CFG_GPI(${parts[0]}, ${term}, ${reset}),"
+            echo -e "\tPAD_CFG_GPI(${parts[0]}, ${term}, ${reset}),"
             ;;
         0x4?000300)
-            echo -e "\t\tPAD_NC(${parts[0]}, ${term}),"
+            echo -e "\tPAD_NC(${parts[0]}, ${term}),"
             ;;
         0x??000400 | 0x??000500 | 0x??000600 | 0x??000700)
-            echo -e "\t\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF1),"
+            echo -e "\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF1),"
             ;;
         0x??000800 | 0x??000900 | 0x??000a00 | 0x??000b00)
-            echo -e "\t\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF2),"
+            echo -e "\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF2),"
             ;;
         0x??000c00 | 0x??000d00 | 0x??000e00 | 0x??000f00)
-            echo -e "\t\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF3),"
+            echo -e "\tPAD_CFG_NF(${parts[0]}, ${term}, ${reset}, NF3),"
             ;;
         *)
-            echo -e "\t\t_PAD_CFG_STRUCT(${parts[0]}, ${parts[1]}, ${parts[2]}),"
+            echo -e "\t_PAD_CFG_STRUCT(${parts[0]}, ${parts[1]}, ${parts[2]}),"
             ;;
     esac
 done
