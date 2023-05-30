f903877cfb11b33d7e283ea52f6f10b9e3950064
Tim Crawford <tcrawford@system76.com>
2023-05-30 17:49:41 -0600

    scripts: Disable showing signature when getting date
    Fixes building when `log.showSignature` is enabled.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,7 +19,7 @@ then
 fi
 MODEL_DIR="$(realpath "models/${MODEL}")"
 
-DATE="$(git show --format="%cd" --date="format:%Y-%m-%d" --no-patch)"
+DATE="$(git show --format="%cd" --date="format:%Y-%m-%d" --no-patch --no-show-signature)"
 REV="$(git describe --always --dirty --abbrev=7)"
 VERSION="${DATE}_${REV}"
 echo "Building '${VERSION}' for '${MODEL}'"
