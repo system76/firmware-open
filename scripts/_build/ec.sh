system76/firmware-open

commit 569321f9ac798277c66948773d6fe710dda9bc64

scripts: Set EC build dir
Specify the EC build directory so that it does not require the model or
version to be determined in the script.

Signed-off-by: Tim Crawford <tcrawford@system76.com>

scripts/_build/ec.sh
@@ -15,7 +15,6 @@ while read line; do
     fi
 done < "$1"
 
-source "$1"
-make -C ec VERSION="${VERSION}" "${EC_ARGS[@]}" clean
-make -C ec VERSION="${VERSION}" "${EC_ARGS[@]}" -j "$(nproc)"
-cp "ec/build/${BOARD}/${VERSION}/ec.rom" "$2"
+make -C ec clean
+make -C ec VERSION="${VERSION}" "${EC_ARGS[@]}" BUILD=build -j "$(nproc)"
+cp "ec/build/ec.rom" "$2"

commit 0bb3f336ddf1002e21b9fe3565c3841a97650c83

scripts: Always set EC build dir
Building EC was broken as the build output from the previous board was
not being cleaned. `BUILD` is now always set, defaulting to "build",
instead of only when `BOARD` is specified. For good measure, add it
to the clean command in case a custom path is used.

Fixes: 569321f9ac79 ("scripts: Set EC build dir")
Signed-off-by: Tim Crawford <tcrawford@system76.com>

scripts/_build/ec.sh
@@ -15,6 +15,8 @@ while read line; do
     fi
 done < "$1"
 
-make -C ec clean
-make -C ec VERSION="${VERSION}" "${EC_ARGS[@]}" BUILD=build -j "$(nproc)"
-cp "ec/build/ec.rom" "$2"
+BUILD_DIR="build"
+
+make -C ec BUILD="$BUILD_DIR" clean
+make -C ec VERSION="${VERSION}" "${EC_ARGS[@]}" BUILD="$BUILD_DIR" -j "$(nproc)"
+cp "ec/$BUILD_DIR/ec.rom" "$2"