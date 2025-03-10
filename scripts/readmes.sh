8ff8a75096ec0168cbb56fe5e3b8d7388b930fa1
Tim Crawford <tcrawford@system76.com>
2025-03-10 16:38:00 -0600

    Remove READMEs that are just directory listings
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- scripts/readmes.sh
+++ scripts/readmes.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-README_DIRS=(
-    apps
-    models
-    tools
-)
-
 set -e
 
 cargo build --release -p modeltool
@@ -24,51 +18,4 @@ readme_model() {
 for dir in models/*/
 do
     readme_model "${dir%/}"
-done
-
-readme_line() {
-    echo -e "  \x1B[1m$1\x1B[0m" >&2
-
-    name="$(basename "$1")"
-    description=""
-
-    readme="$(find "$1" -maxdepth 1 -iname README.md)"
-    if [ -n "$readme" ]
-    then
-        # Get first line, removing the trailing pounds and spaces
-        description=" - $(head -n 1 "$readme" | sed -e 's/^[#[:space:]]*//')"
-    fi
-
-    submodule="$(git submodule status "$1" 2> /dev/null | cut -d ' ' -f 3 || true)"
-    if [ "$submodule" = "$1" ]
-    then
-        # Link to submodule URL
-        origin="$(git -C "$1" remote get-url origin)"
-        echo "- [$name]($origin)$description"
-    else
-        # Link to directory
-        echo "- [$name](./$1)$description"
-    fi
-}
-
-readme_dir() {
-    echo -e "\x1B[1m$1\x1B[0m" >&2
-
-    pushd "$1" > /dev/null
-        cp README.md.in README.md
-
-        echo -e "\n## Contents\n" >> README.md
-        for dir in */
-        do
-            if ! git check-ignore --quiet "${dir}"
-            then
-                readme_line "${dir%/}" >> README.md
-            fi
-        done
-    popd > /dev/null
-}
-
-for dir in "${README_DIRS[@]}"
-do
-    readme_dir "$dir"
 done
